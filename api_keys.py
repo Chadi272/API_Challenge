3e3071403b52f362bfc47bbf88525f519cd86f71
ChadiGhosn <chadighosn272@gmail.com>
2022-07-11 20:44:19 +0100

    Submission

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+#wak = "1a5e98feff8b7557f3519dab8cd07ad2"
+#wak = "60c9f2a2160a756c129843863599ead9"
+#wak = "0a8d3670c3db54c1396e7ad1d1bb160c"
+wak = "c7e84a32a5ff00a7ff9f65665206bc40"
+# Google API Key
+gk = "AIzaSyCaSdZyRyusDhNZFXTWclzXsXbf83LmDhE"
